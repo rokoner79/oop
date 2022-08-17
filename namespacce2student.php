20eaaf024b2605f245568618433704ef734dca7d
rokoner79 <79681506+rokoner79@users.noreply.github.com>
2022-08-18 00:23:21 +0600

    Add files via upload

---
+++ namespacce2student.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\classes;
+class student
+{
+	public $name="Rokon";
+
+}
+
+?>
---
+++ namespace2view.php
@@ -0,0 +1,6 @@
+<?php
+include'./student.php';
+use App\classes\student;
+$obj=new student;
+echo $obj->name; /*Wow method er mto variable keo call kra zay*/
+?>
