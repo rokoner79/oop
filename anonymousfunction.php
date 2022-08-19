cabf11c38392d081979a384f5b69195fd73b5fb7
rokoner79 <79681506+rokoner79@users.noreply.github.com>
2022-08-19 09:40:22 +0600

    Add files via upload

---
+++ anonymousfunction.php
@@ -0,0 +1,8 @@
+<?php
+$variable= function($name){
+	echo"hello $name";
+}; /* zehetu function ti variable e rakci sehetu semiclon dite hbe*/
+
+$variable("rokon");
+
+?>
---
+++ arrayphp.php
@@ -0,0 +1,6 @@
+<?php
+$name=array ('rokon','emn','tarikul');//index array
+echo $name[2]; //op: tarikul
+echo $name;
+
+?>
